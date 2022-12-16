53edee2d8eb78a673d9da6ad0c88c269eb2bd5cc
Ayush Katoch <katochayushhp@gmail.com>
2022-12-16 23:06:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const amountEl_one = document.getElementById('amount-one');
 const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
 
-const rateEl = document.getElementById('rate');
+const rateEl = document.getElementById('rate');//
 const swap = document.getElementById('swap');
 
 // Fetch exchange rates and update the DOM
