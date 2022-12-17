bc9f50edd7a64b86934846d800f9e74dae28c66d
Ayush Katoch <katochayushhp@gmail.com>
2022-12-17 23:44:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function calculate() {
     .then(res => res.json())
     .then(data => {
       // console.log(data);
+    //
       const rate = data.rates[currency_two];
 
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
