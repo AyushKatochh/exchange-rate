fcd94ba34891c5feb37c6c180da0c4947fad366f
Ayush Katoch <katochayushhp@gmail.com>
2022-12-25 22:35:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const swap = document.getElementById('swap');
 
 // Fetch exchange rates and update the DOM
 function calculate() {
-  const currency_one = currencyEl_one.value;
+  const currency_one = currencyEl_one.value;//
   const currency_two = currencyEl_two.value;
 
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
